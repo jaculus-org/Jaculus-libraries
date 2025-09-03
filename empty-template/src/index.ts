30833b4bdedc6536e4891599ae9eef7e6aca43d0
Jakub Andrysek <jakub.andrysek03@gmail.com>
2025-09-03 22:46:40 +0200

    feat: Add deps to tsconfig and example to the index.ts

--- empty-template/src/index.ts
+++ empty-template/src/index.ts
@@ -1,3 +1,9 @@
+import * as gpio from "gpio";
+
 export function add(a: number, b: number): number {
     return a + b;
+}
+
+export function gpioSetOutput(pin: number): void {
+    gpio.pinMode(pin, gpio.PinMode.OUTPUT);
 }
